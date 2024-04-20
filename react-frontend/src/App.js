a223b974def8e801d417dabc904c9c9f849bcd40
agnesajashanicaa <122025593+agnesajashanicaa@users.noreply.github.com>
2024-04-20 14:06:17 +0200

    Creating react header and footer

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
     <div> 
-      <ListEmployeeComponent />
+      <Router>
+      <HeaderComponent />
+      <div className="container">
+      <Switch>
+        <Route exact path="/" component = {ListEmployeeComponent}></Route>
+        <Route path="/employees" component = {ListEmployeeComponent}></Route>
+     </Switch>
+      </div>
+      <FooterComponent />
+      </Router>
     </div>
   );
 }
