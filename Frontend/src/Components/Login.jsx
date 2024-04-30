4c30318c78a959dac2f0607260d876e96eb1a36e
dionamuciqi <diona.muqiqi3@gmail.com>
2024-04-30 17:35:14 +0200

    hnadle input fileds and pass data to server

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import './style.css'
+import axios from 'axios'
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+})
+const handleSubmit = (event) => {
+  event.preventDefault();
+  axios.post('http://localhost:3000/auth/adminlogin')
+  .then(result => console.log(result))
+  .catch(err => console.log(err))
+}
+
+
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
         <div className='p-3 rounded w-25 border loginForm'>
            <h2>Login Page</h2>
-           <form>
+           <form onSubmit = {handleSubmit}>
                 <div className='mb-3'>
-                    <label htmlFor="email"><strong>Email:</strong></label>
-                    <input type="email" name='email' autoComplete='off' placeholder='Enter Email'
-                    className='form-control rounded-0'/>
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor="password"><strong>Password:</strong></label>
-                    <input type="password" name='password' placeholder='Enter Password'
-                    className='form-control rounded-0'/>
-                </div>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder='Enter Email' name='email' 
+                          onChange={e => setValues({...values, email: e.target.value})} className='form-control rounded-0' autoComplete='off'/>
+                  </div>
+                  <div className='mb-3'>
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder='Enter Password' name='password'
+                          onChange={e => setValues({...values, password: e.target.value})} className='form-control rounded-0' />
+                  </div>
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
                 <div className='mb-1'>
                     <input type="checkbox" name="tick" id="tick" className='me-2' />
