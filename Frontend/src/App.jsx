a3e569de351246365ff11d4001978dccdc3e8529
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2024-05-06 21:04:28 +0200

    some changes for domain

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+<Route path='/' element= {<Login />}></Route>
 <Route path='/adminlogin' element= {<Login />}></Route>
 <Route path='/dashboard' element= {<Dashboard />}>
 <Route path='' element={<Home />}></Route>
