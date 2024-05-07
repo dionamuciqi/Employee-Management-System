9df963ce71b472fb3a669f668fbc06b725817b62
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2024-05-07 16:14:11 +0200

    Dashboard Home Design

--- Backend/Routes/AdminRoute.js
+++ Backend/Routes/AdminRoute.js
@@ -124,4 +124,28 @@ router.delete('/delete_employee/:id', (req, res) => {
     })
 })
 
-export { router as adminRouter }
+router.get('/admin_count', (req, res) => {
+    const sql = "SELECT count(id) as admin FROM admin";
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+router.get('/employee_count', (req, res) => {
+    const sql = "SELECT count(id) as employee FROM employee";
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+router.get('/salary_count', (req, res) => {
+    const sql = "SELECT sum(salary) as salary FROM employee";
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+export { router as adminRouter };
